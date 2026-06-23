5be1b952b72897e0ff5e3d8514d2038fecc4f5b0
John Durtche <johndurtche@gmail.com>
2026-06-22 20:00:59 -0400

    button styling

--- darksight/src/App.vue
+++ darksight/src/App.vue
@@ -1,11 +1,49 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <h1>Vite + Vue</h1>
+    <div class="card">
+      <p>
+        Edit <code>src/App.vue</code> and save to test HMR
+      </p>
+      <p>
+        <button>Submit<svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor"><path d="M630-444H192v-72h438L429-717l51-51 288 288-288 288-51-51 201-201Z"/></svg></button>
+      </p>
+      <p>
+        <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m330-444 201 201-51 51-288-288 288-288 51 51-201 201h438v72H330Z"/></svg>Submit</button>
+      </p>
+      <p>
+        <button class="btn-ghost"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m330-444 201 201-51 51-288-288 288-288 51 51-201 201h438v72H330Z"/></svg>Submit</button>
+      </p>
+      <p>
+        <button disabled>Submit</button>
+      </p>
+      <p>
+        <button class="btn-small">Submit</button>
+      </p>
+      <p>
+        <button class="btn-medium">Submit</button>
+      </p>
+      <p>
+        <button class="btn-large">Submit</button>
+      </p>
+      <p>
+        <button class="btn-outline">Submit</button>
+      </p>
+      <p>
+        <button class="btn-icon-only"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m330-444 201 201-51 51-288-288 288-288 51 51-201 201h438v72H330Z"/></svg></button>
+      </p>
+      <p>
+        <button class="btn-outline"><svg class="divided" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m330-444 201 201-51 51-288-288 288-288 51 51-201 201h438v72H330Z"/></svg>Submit</button>
+      </p>
+      <div class="btn-group">
+        <button class="btn">One</button>
+        <button class="btn">Two</button>
+        <button class="btn">Three</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
